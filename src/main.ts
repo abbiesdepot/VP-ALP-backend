7bbafcf3ae1139ad1f87da1b28433ed948881d41
lnehemia <lnehemia@student.ciputra.ac.id>
2025-12-09 10:57:49 +0700

    route, fix bug port

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import express from "express"
+import { PORT } from "./utils/env-util"
+import { publicRouter } from "./routes/public-api"
+import { errorMiddleware } from "./middlewares/error-middleware"
+import { privateRouter } from "./routes/private-api"
+const app = express()
+
+app.use(express.json())
+app.use("/api", publicRouter)
+app.use("/api", privateRouter)
+app.use(errorMiddleware)
+
+app.listen(PORT || 6000, () => {
+    console.log(`Connected to port ${PORT}`)
+})
